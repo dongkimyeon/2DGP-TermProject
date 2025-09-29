d505c4a7a9b9868cdc6b46d872a81c0d7b752fde
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-09-29 19:29:21 +0900

    Banshee 공격할 노트와 인자들 추가

---
+++ Banshee_Attack_note.py
@@ -0,0 +1,30 @@
+from Time import Time
+from ImageManager import ImageManager
+import pico2d
+import SceneManager
+import math
+
+
+class Note:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.width = 0
+        self.height = 0
+        self.speed = 0
+        self.direction = 0
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def shot(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+
--- Enemy_Banshee.py
+++ Enemy_Banshee.py
@@ -1,9 +1,13 @@
-
+from Time import Time
+from ImageManager import ImageManager
+import pico2d
+import SceneManager
+import math
 
 
 class Banshee:
     def __init__(self):
-        self.health = 70
+        self.health = 50
         self.attack_power = 12
         self.x = 0
         self.y = 0
@@ -13,7 +17,10 @@ class Banshee:
         self.state = 'idle'  # 'idle', 'attack', 'hit'
         self.is_hit = False
         self.is_dead = False
-
+        self.width = 50
+        self.height = 50
+        self.detection_radius = 200
+        self.attack_cooldown = 2.0
 
     def attack(self):
         return self.attack_power
@@ -25,6 +32,7 @@ class Banshee:
             damage_taken = 0
         self.health -= damage_taken
         return damage_taken
+
     def set_position(self, x, y):
         self.x = x
         self.y = y
