0b13b1956b2fcb07f63488752d67be964c118f1a
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-10-29 19:50:12 +0900

    카메라 줌 인자 변경

--- Camera.py
+++ Camera.py
@@ -5,7 +5,7 @@ class Camera:
         self.mX = 0.0
         self.mY = 0.0
         self.mTarget = None
-        self.zoom = 1.5  # 확대/축소 비율
+        self.zoom = 2.0  # 확대/축소 비율
 
 
     def update(self):
@@ -14,8 +14,8 @@ class Camera:
             self.mX = self.mTarget.x - 640.0  # 1280 / 2
             self.mY = self.mTarget.y - 360.0  # 720 / 2
             # Clamp to map bounds (2000x2000)
-            self.mX = max(0.0, min(self.mX, 3200.0 - 1920.0))
-            self.mY = max(0.0, min(self.mY, 1600.0 - 1080.0))
+            self.mX = max(0.0, min(self.mX, 3200.0 - 1280.0))
+            self.mY = max(0.0, min(self.mY, 1600.0 - 720.0))
 
     def set_target(self, target):
         self.mTarget = target
