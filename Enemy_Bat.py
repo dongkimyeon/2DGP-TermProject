9fdf1f6e6a3551cf8ce5e0fc09253fa603d2d05b
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-11-19 20:16:16 +0900

    박쥐 충돌처리 추가

--- Enemy_Bat.py
+++ Enemy_Bat.py
@@ -184,4 +184,10 @@ class Bat:
 
     def handle_collision(self, group, other):
         """충돌 처리"""
-        pass
+        if group == 'katana_effect:bat':
+            # 카타나 이펙트와 충돌
+            if other.can_hit(self):  # 아직 맞지 않았다면
+                damage = other.get_damage()
+                self.take_damage(damage)
+                other.mark_hit(self)  # 맞은 것으로 표시
+                print(f"박쥐가 {damage} 데미지를 받았습니다! 남은 체력: {self.health}")
--- Stage1_scene.py
+++ Stage1_scene.py
@@ -92,6 +92,7 @@ class Stage1Scene:
         # 충돌 시 제거할 객체 리스트
         objects_to_remove = []
 
+        # 플레이어와 다른 객체들의 충돌 체크
         left_a, bottom_a, right_a, top_a = player.get_bb()
         for obj in self.gameobjs:
             left_b, bottom_b, right_b, top_b = obj.get_bb()
@@ -130,6 +131,50 @@ class Stage1Scene:
                 # Bullet도 제거 리스트에 추가
                 objects_to_remove.append(obj)
 
+        # 카타나 이펙트와 적들의 충돌 체크
+        if player.katana_effect.active:
+            katana_left, katana_bottom, katana_right, katana_top = player.katana_effect.get_bb()
+
+            for obj in self.gameobjs:
+                if isinstance(obj, Ghost):
+                    obj_left, obj_bottom, obj_right, obj_top = obj.get_bb()
+
+                    # AABB 충돌 체크
+                    if not (katana_left > obj_right or katana_right < obj_left or
+                            katana_top < obj_bottom or katana_bottom > obj_top):
+                        # 충돌 발생
+                        obj.handle_collision('katana_effect:ghost', player.katana_effect)
+
+                        # 체력이 0 이하면 제거 리스트에 추가
+                        if obj.health <= 0:
+                            objects_to_remove.append(obj)
+
+                elif isinstance(obj, Banshee):
+
+
+        # 맵 타일 렌더링 (충돌 박스 표시 활성화)
+                    if not (katana_left > obj_right or katana_right < obj_left or
+
+        # 게임 오브젝트 렌더링
+                        obj.handle_collision('katana_effect:banshee', player.katana_effect)
+
+            left, bottom, right, top = gameobj.get_bb()
+            pico2d.draw_rectangle(
+                (left - self.camera.mX) * self.camera.zoom, (bottom - self.camera.mY) * self.camera.zoom,
+
+                elif isinstance(obj, Bat):
+            )
+
+        player.render(self.camera.mX, self.camera.mY, self.camera.zoom)
+                    if not (katana_left > obj_right or katana_right < obj_left or
+        pico2d.draw_rectangle(
+            (left - self.camera.mX) * self.camera.zoom, (bottom - self.camera.mY) * self.camera.zoom,
+                        obj.handle_collision('katana_effect:bat', player.katana_effect)
+
+
+        # 포탈 근처에 있을 때 UI 표시
+        if player.near_portal:
+
         # 충돌한 객체들을 gameobjs에서 제거
         for obj in objects_to_remove:
             if obj in self.gameobjs:
@@ -140,51 +185,14 @@ class Stage1Scene:
         self.update_mouse_from_events(events)
 
         # 플레이어 이벤트 처리
-                    if not (katana_left > obj_right or katana_right < obj_left or
+        result = player.handel_event(events)
 
         # 포탈 진입 신호 확인
         if result == 'enter_portal':
-
+            print("Entering Stage 2...")
             SceneManager.load_scene("Stage2Scene")
             return True
 
-
-                elif isinstance(obj, Banshee):
-                    obj_left, obj_bottom, obj_right, obj_top = obj.get_bb()
-
-                    # AABB 충돌 체크
-                    if not (katana_left > obj_right or katana_right < obj_left or
-                            katana_top < obj_bottom or katana_bottom > obj_top):
-                        # 충돌 발생
-                        obj.handle_collision('katana_effect:banshee', player.katana_effect)
-
-                        # 체력이 0 이하면 제거 리스트에 추가
-                        if obj.health <= 0:
-                            objects_to_remove.append(obj)
-        return False
-
-    def render(self):
-        # 맵 타일 렌더링 (충돌 박스 표시 활성화)
-        self.map_manager.render(self.camera.mX, self.camera.mY, self.camera.zoom, draw_collision_box=True)
-
-        # 게임 오브젝트 렌더링
-        for gameobj in self.gameobjs:
-            gameobj.render(self.camera.mX, self.camera.mY, self.camera.zoom)
-            left, bottom, right, top = gameobj.get_bb()
-            pico2d.draw_rectangle(
-                (left - self.camera.mX) * self.camera.zoom, (bottom - self.camera.mY) * self.camera.zoom,
-                (right - self.camera.mX) * self.camera.zoom, (top - self.camera.mY) * self.camera.zoom
-            )
-        # 플레이어 렌더링
-        player.render(self.camera.mX, self.camera.mY, self.camera.zoom)
-        left, bottom, right, top = player.get_bb()
-        pico2d.draw_rectangle(
-            (left - self.camera.mX) * self.camera.zoom, (bottom - self.camera.mY) * self.camera.zoom,
-            (right - self.camera.mX) * self.camera.zoom, (top - self.camera.mY) * self.camera.zoom
-        )
-
-        # 포탈 근처에 있을 때 UI 표시
-        if player.near_portal:
             font = ResourceManager.get_font("default")
             font.draw(SceneManager.screen_width // 2 - 100, SceneManager.screen_height - 50,
                      "Press F to Enter", (255, 255, 255))
