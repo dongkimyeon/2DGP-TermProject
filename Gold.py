eaf376921311486bb468c875ad3803d0a9348f49
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-11-25 01:03:56 +0900

    gold 크기 수정

--- Gold.py
+++ Gold.py
@@ -5,8 +5,8 @@ class Gold:
     def __init__(self, x,y):
         self.x = x
         self.y = y
-        self.width = 7
-        self.height = 7
+        self.width = 10
+        self.height = 10
         self.scale = 2.0
         self.frame_count = 0
         self.frame_timer = 0.0
@@ -15,7 +15,7 @@ class Gold:
     def get_bb(self):
         half_width = self.width // 2
         half_height = self.height // 2
-        return (self.x - half_width * self.scale, self.y - half_height * self.scale + 10, self.x + half_width * self.scale, self.y + half_height * self.scale + 10)
+        return (self.x - half_width * self.scale, self.y - half_height * self.scale, self.x + half_width * self.scale, self.y + half_height * self.scale)
 
     def update(self):
         dt = Time.DeltaTime()
